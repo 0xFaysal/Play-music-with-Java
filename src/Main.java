391b3287411cc31956b7f784b1fcc3d3d789b289
Faysal Ahmed Fahim <ffahim2330717@bscse.uiu.ac.bd>
2024-10-03 22:19:13 +0600

    music playing functionality is added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
+        File music = new File("Ready.wav");
+        AudioInputStream AIn = AudioSystem.getAudioInputStream(music);
+        Clip clip = AudioSystem.getClip();
+        clip.open(AIn);
+        clip.start();
+        long lengthInMicroseconds = clip.getMicrosecondLength();
+        long lengthInMilliseconds = lengthInMicroseconds / 1000;
+        System.out.println("Length of the audio file in Microseconds: " + lengthInMicroseconds);
+        System.out.println("Length of the audio file in milliseconds: " + lengthInMilliseconds);
+
+
+        Thread.sleep(lengthInMicroseconds);
     }
 }
